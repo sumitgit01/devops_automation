df9b9995ea1320c0fa9eabfa187a51e5f1e0032b
sumitgit01 <sumitjoshi1988@gmail.com>
2024-06-15 21:49:53 +0530

    agent updated

--- vars/build_and_deployment_sit.groovy
+++ vars/build_and_deployment_sit.groovy
@@ -1,6 +1,5 @@
 pipeline {
-    agent any
-
+    agent agent1
  tools{
      jdk 'jdk17'
      maven 'maven3'
@@ -9,6 +8,19 @@ pipeline {
         stage('Which Java?') {
             steps {
                 sh 'java --version'
+            }
+        }
+    stage('checkout SCM') {
+            steps {
+                sh 'java --version'
+            }
+        }
+    stage('build provisioning') {
+            steps {
+                sh """
+                mvn clean install
+                ls -alrt
+                """
             }
         }
     }
