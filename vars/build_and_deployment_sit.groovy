b263d1602dd7dcc33d491c62763099418dcf4a0c
sumitgit01 <sumitjoshi1988@gmail.com>
2024-07-04 22:01:15 +0530

    build_and_deployment_sit.groovy :new checkout method implemented

--- vars/build_and_deployment_sit.groovy
+++ vars/build_and_deployment_sit.groovy
@@ -24,7 +24,19 @@ pipeline {
                     cd seh-students/
                     #git checkout feature/devops
                     """ */
-                    git branch: 'main', url: "${bit_bucket_repo_url}", credentialsId: "${bitBucketCredentialsid}"
+                    //git branch: 'main', url: "${bit_bucket_repo_url}", credentialsId: "${bitBucketCredentialsid}"
+                    checkout([$class: 'GitSCM',
+                    branches: [[name: 'main']],
+                    userRemoteConfigs: [[url: "${bit_bucket_repo_url}", credentialsId: "${bitBucketCredentialsid}"]],
+                    extensions: [[$class: 'CloneOption', depth: 1, noTags: false, reference: '', shallow: true, timeout: 120]]])
+                    withCredentials([usernameColonPassword(credentialsId: "${bitBucketCredentialsid}", passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]){
+                        sh """
+                        export GIT_COMMITTER_NAME='CICD_SEH'
+                        export GIT_COMMITTER_EMAIL='sumitjoshi1988@gmail.com'
+                        git config http.sslverify false 
+                        """
+                    }
+
 
 
                 }
@@ -41,7 +53,8 @@ pipeline {
         }
         stage('deploy app') {
             steps {
-                dir("${WORKSPACE}/seh-students/target"){
+                //dir("${WORKSPACE}/seh-students/target")
+                dir("${WORKSPACE}/target"){
                     sh """
                         chmod +x seh-students.jar
                         java -jar seh-students.jar
